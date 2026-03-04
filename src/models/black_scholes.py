b4adf5fab2814a2f04a03e4385be142c12c87379
Baha <baha.d.naifar@gmail.com>
2026-03-04 15:51:15 +0100

    Add: Black-Scholes Model class

---
+++ src/models/black_scholes.py
@@ -0,0 +1,17 @@
+import numpy as np
+from scipy.stats import norm
+from option import Option
+
+class BlackScholes:
+    def __init__(self, option: Option):
+        self.option = option
+
+    def price(self):
+        d1 = (np.log(self.option.S0 / self.option.K) + (self.option.r + 0.5 * self.option.sigma ** 2) * self.option.T) / (self.option.sigma * np.sqrt(self.option.T))
+        d2 = d1 - self.option.sigma * np.sqrt(self.option.T)
+        if (self.option.option_type == "call"):
+            C = self.option.S0 * norm.cdf(d1) - self.option.K * np.exp((-self.option.r) * self.option.T) * norm.cdf(d2)
+            return C
+        elif (self.option.option_type == "put"):
+            C = self.option.K * np.exp((-self.option.r) * self.option.T) * norm.cdf(-d2) - self.option.S0 * norm.cdf(-d1)
+            return C
